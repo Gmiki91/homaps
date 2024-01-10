4e1a87198b9b989089b0956680886c14fc91e30e
Miklos Gabor <gmiki91@gmail.com>
2024-01-10 10:26:42 +0100

    no dupliacte events on same day

--- src/EventForm.tsx
+++ src/EventForm.tsx
@@ -9,39 +9,49 @@ type Props = {
 }
 export default function EventForm({ habit, onSubmit }: Props) {
   const [startDate, setStartDate] = useState(new Date());
-  const [formData, setFormData] = useState({qty:0} as FormData);
+  const [formData, setFormData] = useState({ qty: 0 } as FormData);
   type FormData = {
     fullDate: number,
     dayOfYear: number,
     note: string,
-    qty?: number ,
+    qty?: number,
   }
   const handleSubmit = async (event: React.ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const year = startDate.getFullYear();
-    const myEvent: MyEvent = {
-      fullDate: startDate.valueOf(),
-      dayOfYear: Math.floor((startDate.valueOf() - new Date(year, 0, 0).valueOf()) / (1000 * 60 * 60 * 24)),
-      note: formData.note,
-      qty: formData.qty||0,
+    let error = false;
+    for (let i = 0; i < habit.events.length; i++) {
+      if (habit.events[i].fullDate === startDate.setHours(0, 0, 0, 0).valueOf()) {
+        alert("There is already an event on this date.");
+        error = true;
+        break;
+      }
     }
-    axios.post(`http://localhost:4040/${habit._id}`, myEvent).then(() => {
-      setFormData({
-        fullDate: Date.now(),
-        dayOfYear: 0,
-        note: "",
-        qty: 0
-      } as FormData);
-      setStartDate(new Date());
-      onSubmit();
-    })
+    if (!error) {
+      const year = startDate.getFullYear();
+      const myEvent: MyEvent = {
+        fullDate: startDate.setHours(0, 0, 0, 0).valueOf(),
+        dayOfYear: Math.floor((startDate.valueOf() - new Date(year, 0, 0).valueOf()) / (1000 * 60 * 60 * 24)),
+        note: formData.note,
+        qty: formData.qty || 0,
+      }
+      axios.post(`http://localhost:4040/${habit._id}`, myEvent).then(() => {
+        setFormData({
+          fullDate: Date.now(),
+          dayOfYear: 0,
+          note: "",
+          qty: 0
+        } as FormData);
+        setStartDate(new Date());
+        onSubmit();
+      }).catch(error => alert(error.message));
+    }
   }
   const handleChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
     const { name, value } = (event.target);
     setFormData(prevData => ({ ...prevData, [name]: value }))
   }
   const measurement = habit.measure ? <div className="measurement">
-    <input autoComplete="off" className="unit" type="text"  name="qty" value={formData.qty} onChange={handleChange}  />
+    <input autoComplete="off" className="unit" type="text" name="qty" value={formData.qty} onChange={handleChange} />
     <label>
       {habit.unit}
     </label>
@@ -58,8 +68,8 @@ export default function EventForm({ habit, onSubmit }: Props) {
         autoComplete="off"
       />
       <div className="flex_row">
-      {measurement}
-      <input className="button_8" type="submit" value="Submit" />
+        {measurement}
+        <input className="button_8" type="submit" value="Submit" />
       </div>
     </form>
   </div>
